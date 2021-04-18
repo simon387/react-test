1c9a3833543dbb9f1d614e321361fad8b1636420
simone celia <simone.celia@thinkopen.it>
2021-04-19 00:15:58 +0200

    new-less

--- last/monster-rolodex/src/App.js
+++ last/monster-rolodex/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import { CardList} from "./components/card-list/card-list.component";
 
 class App extends Component {
 	constructor() {
 		super();
+
 		this.state = {
 			monsters: []
 		};
@@ -18,11 +20,13 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				{
-					this.state.monsters.map(monster => (
-						<h1 key={monster.id}>{monster.name}</h1>
-					))
-				}
+				<CardList name={"simone"}>
+					{
+						this.state.monsters.map(monster => (
+							<h1 key={monster.id}>{monster.name}</h1>
+						))
+					}
+				</CardList>
 			</div>
 		);
 	}
