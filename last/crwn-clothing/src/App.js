39bfdc8333d56ef07f2437a2e8b3f3b1ebb7448c
simone celia <simone.celia@thinkopen.it>
2021-05-08 01:27:53 +0200

    new_antes_sign_up

--- last/crwn-clothing/src/App.js
+++ last/crwn-clothing/src/App.js
@@ -21,9 +21,20 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-			// this.setState({currentUser: user});
-			await createUserProfileDocument(user);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot(snapshot => {
+					this.setState({
+						currentUser: {
+							id: snapshot.id,
+							...snapshot.data()
+						}
+					})
+				});
+			}
+			this.setState({currentUser: userAuth});
 		});
 	}
 
