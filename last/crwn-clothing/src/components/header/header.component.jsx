7ec843d2c018ff561dde284e63d98606ca69ab72
simone <simon387@hotmail.it>
2021-05-14 01:25:04 +0200

    new lesson

--- last/crwn-clothing/src/components/header/header.component.jsx
+++ last/crwn-clothing/src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 
-import {auth} from '../../firebase/firebase.util';
+import {auth} from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
--- last/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ last/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.components";
 
-import {signInWithGoogle} from "../../firebase/firebase.util";
+import {signInWithGoogle} from "../../firebase/firebase.utils";
 
 import './sign-in.styles.scss';
 
