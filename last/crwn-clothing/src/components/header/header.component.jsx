c9bf6f22eca8b261f1f277270bc81987807f96ad
simone <simon387@hotmail.it>
2021-05-16 18:29:17 +0200

    cart icon component

--- last/crwn-clothing/src/components/header/header.component.jsx
+++ last/crwn-clothing/src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import {connect} from "react-redux";
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import './header.styles.scss';
 
@@ -22,6 +23,7 @@ const Header = ({currentUser}) => (
 					:
 					<Link className='option' to='/signin'>SIGN IN</Link>
 			}
+			<CartIcon/>
 		</div>
 	</div>
 );
