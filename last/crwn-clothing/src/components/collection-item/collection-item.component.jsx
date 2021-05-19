23195e0a21c81863e0b016b863a8892debe8a146
simone celia <simone.celia@thinkopen.it>
2021-05-19 18:48:58 +0200

    new_lex

--- last/crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ last/crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {connect} from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.components";
+import {addItem} from "../../redux/cart/cart.action";
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-	<div className='collection-item'>
+const CollectionItem = ({item, addItem}) => {
+	const {name, price, imageUrl} = item;
+	return (
+		<div className='collection-item'>
 		<div className='image'
 		     style={{
 			     backgroundImage: `url(${imageUrl})`
@@ -15,8 +19,14 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
 			<span className='name'>{name}</span>
 			<span className='price'>{price}</span>
 		</div>
-		<CustomButton inverted>Add to cart</CustomButton>
-	</div>
-);
+		<CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
+	</div>)
+};
 
-export default CollectionItem;
+
+
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem())
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
