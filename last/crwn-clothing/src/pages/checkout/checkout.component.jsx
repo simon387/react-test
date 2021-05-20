a997acdaa92c02f5c98c411f0eb211b6c8d2c3ff
simone celia <simone.celia@thinkopen.it>
2021-05-20 21:55:42 +0200

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- last/crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ last/crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import {connect} from "react-redux";
 import {createStructuredSelector} from "reselect";
 
+import CheckoutItem from "../../components/checkout-item/chekout-item.component";
+
 import {selectCartItems} from "../../redux/cart/cart.selector";
 import {selectCartTotal} from "../../redux/cart/cart.selector";
 
@@ -28,7 +30,7 @@ const CheckoutPage = ({cartItems, total}) => (
 		</div>
 		{
 			cartItems.map(cartItem =>
-				cartItem.name
+				<CheckoutItem key={cartItem.id} cartItem={cartItem}/>
 			)
 		}
 		<div className='total'>
